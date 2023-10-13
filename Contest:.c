8f6f4ee9a479f4b7304285e0b24b6e15195534ca
sudheerimmidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2023-10-13 11:27:50 +0530

    #include<stdio.h>
    int main()
    {
        int X,A,B;
        scanf("%d%d%d",&X,&A,&B);
        int sum = A*1 + B*2;
        if(sum>= X)
        {
            printf("Qualify");
        } 
        else 
        {
            printf("Not Qualify");
        }
    }

---
+++ Contest:.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int X,A,B;
+    scanf("%d%d%d",&X,&A,&B);
+    int sum = A*1 + B*2;
+    if(sum>= X)
+    {
+        printf("Qualify");
+    } 
+    else 
+    {
+        printf("Not Qualify");
+    }
+}
