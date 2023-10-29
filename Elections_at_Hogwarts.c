89e97cdd4c1a96f5374d7d6469d7d92b1889729f
sudheerimmidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2023-10-29 18:22:38 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     if(x>50)     {         printf("Gryffindor");     }     else if(y>50)     {         printf("Slytherin");     }     else if(z>50)     {         printf("Hufflepuff");     }     else if(x==y || y==z || z==x)     {         printf("NOTA");     }     else     {         printf("NOTA");     } }

---
+++ Elections_at_Hogwarts.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    if(x>50)
+    {
+        printf("Gryffindor");
+    }
+    else if(y>50)
+    {
+        printf("Slytherin");
+    }
+    else if(z>50)
+    {
+        printf("Hufflepuff");
+    }
+    else if(x==y || y==z || z==x)
+    {
+        printf("NOTA");
+    }
+    else
+    {
+        printf("NOTA");
+    }
+}
