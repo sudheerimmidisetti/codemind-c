0638993b739d0515155469fb6d1c88ab14cc3650
sudheerimmidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2023-12-22 09:36:34 +0530

    #include<stdio.h> int main() {     int n,c=0;     scanf("%d",&n);     int i,arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(i%2==0)         {
                if(arr[i]%2!=0)
                {
                    c++;
                }
            }
        }
        if(c>=1)     printf("False");
        else        printf("True");
    }

---
+++ Strictly_ODD.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    int i,arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(i%2==0)
+        {
+            
+            if(arr[i]%2!=0)
+            {
+                c++;
+            }
+        }
+    }
+    if(c>=1)     printf("False");
+    else        printf("True");
+}
