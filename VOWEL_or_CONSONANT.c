053ae96b3fc01ac277daf544b87ac335431b1d69
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-01-05 11:42:33 +0530

    #include<stdio.h> int main() {    char c;    scanf("%c",&c);    if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')    {        printf("VOWEL");    }    else if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U')    {        printf("VOWEL");    }    else    {        printf("CONSONANT");    } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+   char c;
+   scanf("%c",&c);
+   if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+   {
+       printf("VOWEL");
+   }
+   else if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+   {
+       printf("VOWEL");
+   }
+   else
+   {
+       printf("CONSONANT");
+   }
+}
