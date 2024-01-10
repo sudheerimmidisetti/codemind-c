3ae5bdbe53472630b44203e9d8c5923309104e9b
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-01-10 18:00:38 +0530

    #include<stdio.h> #include<string.h> int main() {     int n;     scanf("%d",&n);     int i,sum=0;     for(i=0;i<n;i++)     {         char s[1000];         scanf("%s",s);         int l=strlen(s);         if(s[0]=='+' || s[l-1]=='+')         {             sum=sum+1;         }         if(s[0]=='-'||s[l-1]=='-')         {             sum=sum-1;         }     }     printf("%d",sum); }

---
+++ Bits++.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,sum=0;
+    for(i=0;i<n;i++)
+    {
+        char s[1000];
+        scanf("%s",s);
+        int l=strlen(s);
+        if(s[0]=='+' || s[l-1]=='+')
+        {
+            sum=sum+1;
+        }
+        if(s[0]=='-'||s[l-1]=='-')
+        {
+            sum=sum-1;
+        }
+    }
+    printf("%d",sum);
+}
